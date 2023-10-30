c211dd54218af4072973fb9e7cc07f3ffdfa45f5
Adevalter <adevalter@gmail.com>
2023-10-30 08:30:12 -0300

    Projeto iniciado - Criado Pacotes

---
+++ app/src/main/java/com/gptecnologia/apicommvp/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.gptecnologia.apicommvp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
